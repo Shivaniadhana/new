5f88db2fa57acd7b4d4f6e1bc88bb27ce2fbbf2c
Shivaniadhana <107605917+Shivaniadhana@users.noreply.github.com>
2022-08-08 12:05:30 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,20 @@
+package lab04_08;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		
+		System.out.println("\t\t\t\t\t\t\t\t\tFibonacci Series");
+		//creating object 
+		Fibonaci obj1 = new Fibonaci(10);
+		obj1.start();
+		
+		System.out.println("\t\t\t\t\t\t\t\t\tRiverse Order ");
+		//creating object 
+		Riverse obj2 = new Riverse(124);
+		obj2.start();
+	}
+	
+
+}
